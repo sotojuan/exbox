c6721869d75718c7c75e618583d2009260e23dee
Juan Soto <juan@juansoto.me>
2016-10-03 12:03:18 -0400

    Use strings instead of characters for boxes

--- lib/exbox/boxes.ex
+++ lib/exbox/boxes.ex
@@ -5,67 +5,67 @@ defmodule ExBox.Boxes do
 
   def single do
     %{
-      :top_left => '┌',
-      :top_right => '┐',
-      :bottom_right => '┘',
-      :bottom_left => '└',
-      :vertical => '│',
-      :horizontal => '─'
+      :top_left => "┌",
+      :top_right => "┐",
+      :bottom_right => "┘",
+      :bottom_left => "└",
+      :vertical => "│",
+      :horizontal => "─"
     }
   end
 
   def double do
     %{
-      :top_left => '╭',
-      :top_right => '╮',
-      :bottom_right => '╯',
-      :bottom_left => '╰',
-      :vertical => '│',
-      :horizontal => '─'
+      :top_left => "╭",
+      :top_right => "╮",
+      :bottom_right => "╯",
+      :bottom_left => "╰",
+      :vertical => "│",
+      :horizontal => "─"
     }
   end
 
   def round do
     %{
-      :top_left => '╔',
-      :top_right => '╗',
-      :bottom_right => '╝',
-      :bottom_left => '╚',
-      :vertical => '║',
-      :horizontal => '═'
+      :top_left => "╔",
+      :top_right => "╗",
+      :bottom_right => "╝",
+      :bottom_left => "╚",
+      :vertical => "║",
+      :horizontal => "═"
     }
   end
 
   def single_double do
     %{
-      :top_left => '╓',
-      :top_right => '╖',
-      :bottom_right => '╜',
-      :bottom_left => '╙',
-      :vertical => '║',
-      :horizontal => '─'
+      :top_left => "╓",
+      :top_right => "╖",
+      :bottom_right => "╜",
+      :bottom_left => "╙",
+      :vertical => "║",
+      :horizontal => "─"
     }
   end
 
   def double_single do
     %{
-      :top_left => '╒',
-      :top_right => '╕',
-      :bottom_right => '╛',
-      :bottom_left => '╘',
-      :vertical => '│',
-      :horizontal => '═'
+      :top_left => "╒",
+      :top_right => "╕",
+      :bottom_right => "╛",
+      :bottom_left => "╘",
+      :vertical => "│",
+      :horizontal => "═"
     }
   end
 
   def classic do
     %{
-      :top_left => '+',
-      :top_right => '+',
-      :bottom_right => '+',
-      :bottom_left => '+',
-      :vertical => '|',
-      :horizontal => '-'
+      :top_left => "+",
+      :top_right => "+",
+      :bottom_right => "+",
+      :bottom_left => "+",
+      :vertical => "|",
+      :horizontal => "-"
     }
   end
 end
