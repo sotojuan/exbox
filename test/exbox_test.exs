a430042de6fde9575b4f6e5d7ffdc3350dc8f7b6
Juan Soto <juan@juansoto.me>
2016-10-08 22:40:11 -0400

    Fix failing tests

--- test/exbox_test.exs
+++ test/exbox_test.exs
@@ -74,14 +74,14 @@ defmodule ExBoxTest do
   end
 
   test "border color option" do
-    expected = "\e[38;5;3m┌─────┐\e[0m\n\e[38;5;3m│\e[0mhello\e[38;5;3m│\e[0m\n\e[38;5;3m└─────┘\e[0m"
+    expected = "\e[38;5;3m┌─────┐\e[39m\n\e[38;5;3m│\e[39mhello\e[38;5;3m│\e[39m\n\e[38;5;3m└─────┘\e[39m"
     actual = ExBox.get("hello", border_color: :yellow) |> IO.iodata_to_binary
 
     assert actual == expected
   end
 
   test "background color option" do
-    expected = "┌─────┐\n│\e[48;5;1mhello\e[0m│\n└─────┘"
+    expected = "┌─────┐\n│\e[48;5;1mhello\e[49m│\n└─────┘"
     actual = ExBox.get("hello", bg_color: :red) |> IO.iodata_to_binary
 
     assert actual == expected
