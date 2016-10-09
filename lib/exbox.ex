7d98f4bde19a05650db870cf9568bc2a7e456418
Juan Soto <juan@juansoto.me>
2016-10-08 21:46:31 -0400

    Add comments

--- lib/exbox.ex
+++ lib/exbox.ex
@@ -26,6 +26,7 @@ defmodule ExBox do
       float: :left
     ]
 
+    # Merge given options with default, preferring the given
     opts = Keyword.merge(default, opts)
 
     {cols, _} = TermSize.get
@@ -33,18 +34,24 @@ defmodule ExBox do
     padding = get_list(opts[:padding])
     margin = get_list(opts[:margin])
 
+    # Align the text, handling possible ANSI codes
     text = ANSI.align(text, [align: opts[:align]])
+    # Split the lines and add given padding to the top or bottom
     lines = text |> String.split("\n") |> fill_lines(padding)
 
+    # Get the width of the widest line and add the left and right padding
     content_width = Width.widest_line(text) + padding[:left] + padding[:right]
     padding_left = duplicate(@pad, padding[:left])
+    # Overwrite left margin depending on given float options (center and right floats need specific numbers)
     margin_left = align_left(margin[:left], opts[:float], content_width, cols)
 
+    # These lines take care of the borders, sizing them to fit the given text
     horizontal = duplicate(chars.horizontal, content_width)
     top = colorize_border([duplicate(@nl, margin[:top]), margin_left, chars.top_left, horizontal, chars.top_right], opts)
     bottom = colorize_border([margin_left, chars.bottom_left, horizontal, chars.bottom_right, duplicate(@nl, margin[:bottom])], opts)
     side = colorize_border(chars.vertical, opts)
 
+    # For each line, add left margin, the side border, padding, text, and the ending side border
     middle =
       lines
       |> Enum.map(
@@ -58,22 +65,28 @@ defmodule ExBox do
   end
 
   defp get_list(detail) when is_number(detail) do
+    # If padding or margin was given as a number, return a list with some default values
     [top: detail, right: detail * 3, bottom: detail, left: detail * 3]
   end
 
   defp get_list(detail) when is_list(detail) do
+    # If padding or margin was given as a list, merge it with some default values
     default = [top: 0, right: 0, bottom: 0, left: 0]
 
     Keyword.merge(default, detail)
   end
 
   defp get_border_chars(style) when is_atom(style) do
+    # Return the box style by calling the given function in the `Boxes` module
+
+    # For example, an argument of `:classic` will return the call to `Boxes.classic`
     apply(Boxes, style, [])
   end
 
   defp colorize_border(x, opts) do
     case Keyword.fetch(opts, :border_color) do
       {:ok, c} ->
+        # Apply the given color `c` to the string representation of the border characters
         apply(ExChalk, c, [IO.iodata_to_binary(x)])
       :error ->
         x
@@ -83,6 +96,7 @@ defmodule ExBox do
   defp colorize_content(x, opts) do
     case Keyword.fetch(opts, :bg_color) do
       {:ok, c} ->
+        # Apply the given color `c` to the background of the content
         color =
           "bg_"
           |> String.to_charlist
@@ -96,6 +110,8 @@ defmodule ExBox do
   end
 
   defp duplicate(x, n) do
+    # Wrapper over `duplicate` that accepts a string and puts it in a List
+    # This is done because we need the result to be a List
     List.duplicate([x], n)
   end
 
@@ -107,6 +123,9 @@ defmodule ExBox do
   end
 
   defp align_left(margin_left, float, content_width, cols) do
+    # Returns a new left margin depending on arguments given
+    # Center and right floats require the correct amount of margin
+
     case float do
       :center ->
         pad_width = div((cols - content_width), 2)
